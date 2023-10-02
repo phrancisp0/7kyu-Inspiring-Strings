e1b3a65f990c233a7fbe99ecad6661a989f266e9
Francis Patawah <phrancisp0@gmail.com>
2023-10-02 01:08:58 -0400

    inspiring strings 7kyu

---
+++ Kata.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Kata {
+  public static String longestWord(String words) {
+    return Arrays.stream(words.split(" "))
+          .reduce((x, y) -> x.length() <= y.length() ? y : x).get();
+  }
+}
+/*
+When given a string of space separated words, return the word with the longest length. If there are multiple words with the longest length, return the last instance of the word with the longest length.
+
+Example:
+
+'red white blue' //returns string value of white
+
+'red blue gold' //returns gold
+*/
